e83f9e738638e77835b5f7c65011f3134fea9d8e
Michał Zieliński <michal@zielinscy.org.pl>
2015-03-30 15:05:40 +0200

    retrieve submit status, caching

--- satori.py
+++ satori.py
@@ -6,13 +6,17 @@ import json
 import os
 import io
 import requests
-import tempfile
 import webbrowser
+import random
+import sys
+import time
+import subprocess
 
 from pyquery import PyQuery as pq
 from lxml.html import etree
 
 BASE = 'https://satori.tcs.uj.edu.pl'
+LOG_REQUESTS = True
 
 def parse_html(html):
     parser = etree.HTMLParser()
@@ -27,12 +31,65 @@ def match_code(query, code):
     code = code.lower()
     return query == code or query + '*' == code
 
+class Cache(object):
+    def __init__(self, path):
+        self.path = path
+        self.data = None
+
+    def load(self):
+        if self.data is not None:
+            return
+        try:
+            with open(self.path) as f:
+                self.data = json.load(f)
+        except IOError:
+            self.data = {}
+
+    def save(self):
+        suf = '.tmp%d' % random.randrange(10000)
+        with open(self.path + suf, 'w') as f:
+            json.dump(self.data, f)
+        os.rename(self.path + suf, self.path)
+
+    def __setitem__(self, key, value):
+        self.load()
+        self.data[key] = value
+        self.save()
+
+    def __getitem__(self, key):
+        self.load()
+        return self.data[key]
+
+    def __contains__(self, key):
+        self.load()
+        return key in self.data
+
+    def get(self, key, default=None):
+        self.load()
+        return self.data.get(key, default)
+
+def cached(cache_name):
+    def wrapper1(func):
+        def wrapper(self, *args):
+            key = repr(args)
+            cache = getattr(self, cache_name)
+            if key not in cache:
+                cache[key] = func(self, *args)
+            return cache[key]
+
+        return wrapper
+
+    return wrapper1
+
 class Session(object):
     def __init__(self, path='~/.config/satori.json',
                  cache_path='~/.cache/satori'):
         self.path = os.path.expanduser(path)
         self.cache_path = os.path.expanduser(cache_path)
         self.settings = {}
+        self.match_contest_cache = Cache(self.cache_path + '/match_contest.json')
+        self.match_submit_problem_cache = Cache(self.cache_path + '/match_submit_problem.json')
+        self.match_problem_cache = Cache(self.cache_path + '/match_problem.json')
 
     def load(self):
         try:
@@ -55,10 +112,15 @@ class Session(object):
     def request(self, method, path, parse=True):
         if not 'satori_token' in self.settings:
             self._login()
+        if LOG_REQUESTS:
+            sys.stderr.write('{} {}...'.format(method, path))
+            sys.stderr.flush()
         r = requests.request(method, BASE + path,
                              headers={'Cookie':
                                       'satori_token='
                                       + self.settings['satori_token']})
+        if LOG_REQUESTS:
+            sys.stderr.write('\b\b\b -> {}\n'.format(r.status_code))
         if parse:
             return pq(r.content)
         else:
@@ -92,6 +154,7 @@ class Session(object):
                 id = link.split('/')[2]
                 yield name, int(id)
 
+    @cached('match_contest_cache')
     def match_contest(self, query):
         try:
             return int(query)
@@ -145,6 +208,7 @@ class Session(object):
             print u'{:<9} {: <5} {}' \
                 .format(id or '', code, desc)
 
+    @cached('match_submit_problem_cache')
     def match_submit_problem(self, contest, problem):
         try:
             return int(problem)
@@ -156,6 +220,7 @@ class Session(object):
                 return (id, code)
         raise SatoriError('unknown problem %r' % problem)
 
+    @cached('match_problem_cache')
     def match_problem(self, contest, problem):
         try:
             return int(problem)
@@ -182,16 +247,57 @@ class Session(object):
         data = self.request('GET', pdf, parse=False)
         return self.cache_write('%d.pdf' % id, data)
 
+    def get_status(self, contest, id):
+        url = '/contest/%d/results/%d' % (self.match_contest(contest), id)
+        body = self.request('GET', url)
+        header = body.find('table.results').find('tr')[1]
+        header = pq(header).find('td')
+
+        problem = pq(header[2]).text()
+        status = pq(header[-1]).text()
+
+        tests = []
+        for row in body.find('.mainsphinx table.docutils tr')[1:]:
+            row = pq(row)
+            cols = map(pq, row.find('td'))
+            tests.append((cols[0].text(), cols[1].text()))
+
+        return problem, status, tests
+
+    def print_status(self, contest, id):
+        problem, status, tests = self.get_status(contest, id)
+        print 'Test report for %s' % problem
+        print 'Status:', status
+        for name, status in tests:
+            print ' - {: <10} {}'.format(name, status)
+
+def notify_status(problem, status):
+    subprocess.check_call(['notify-send', 'New status for {}: {}'.format(problem, status)])
+
+def wait(sess, contest, id):
+    contest = sess.match_contest(contest)
+    last_status = ''
+    while True:
+        problem, status, tests = sess.get_status(contest, id)
+        if status != last_status:
+            notify_status(problem, status)
+            last_status = status
+        if status and status != 'QUE':
+            break
+        time.sleep(10)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='command')
     subparsers.add_parser(
         'login',
         help='Set Satori credentials.')
+
     contests_parser = subparsers.add_parser(
         'contests',
         help='List contests')
     contests_parser.add_argument('--show-other', action='store_true')
+
     problems_parser = subparsers.add_parser(
         'problems',
         help='List problems.')
@@ -199,6 +305,7 @@ if __name__ == '__main__':
                                  action='store_true',
                                  help='show submittable problems.')
     problems_parser.add_argument('contest')
+
     problem_parser = subparsers.add_parser(
         'problem',
         help='Open problem statement.')
@@ -206,6 +313,22 @@ if __name__ == '__main__':
     problem_parser.add_argument('problem')
     problem_parser.add_argument('--pdf', action='store_true',
                                 help='Open PDF instead of problem page.')
+
+    status_parser = subparsers.add_parser(
+        'status',
+        help='Show submit status.')
+    status_parser.add_argument('contest')
+    status_parser.add_argument('id', type=int)
+
+    wait_parser = subparsers.add_parser(
+        'wait',
+        help='Wait for submit status change and show notification.')
+    wait_parser.add_argument('contest')
+    wait_parser.add_argument('id', type=int)
+
+    subparsers.add_parser(
+        'clear-cache',
+        help='Clear cache.')
 
     ns = parser.parse_args()
     sess = Session()
@@ -240,3 +363,14 @@ if __name__ == '__main__':
                 open_pdf()
             else:
                 webbrowser.open(BASE + url)
+
+    elif ns.command == 'status':
+        sess.print_status(ns.contest, ns.id)
+
+    elif ns.command == 'wait':
+        wait(sess, ns.contest, ns.id)
+
+    elif ns.command == 'clear-cache':
+        path = os.path.expanduser('~/.cache/satori')
+        for file in os.listdir(path):
+            os.unlink(path + '/' + file)
